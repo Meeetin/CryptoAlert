23c53b6d8d77b67dc53c4aabc6920442580cb4d2
Edvin Christenson <edvin.christenson@gmail.com>
2019-04-10 17:10:32 +0200

    modified:   app.py     connected api and prints out current price

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-import coinmarketcap
 import json
-
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 with open("index/data.json") as f:
     data = json.load(f)
 
 print(data["key"])
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+      'start': '1',
+      'limit': '2',
+      'convert': 'USD',
+  }
+headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': data["key"],
+  }
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
