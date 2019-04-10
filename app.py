adb12203e07f431ef1a4e72a6d49025c964b23c1
Edvin Christenson <edvin.christenson@gmail.com>
2019-04-10 17:42:46 +0200

    modified:   app.py     Connected API with app, can print out coindata.

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 import json
+import pprint
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 with open("index/data.json") as f:
     data = json.load(f)
-
 print(data["key"])
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info'
 parameters = {
-      'start': '1',
-      'limit': '2',
-      'convert': 'USD',
+      'id' : '1,2',
+     # 'convert': 'USD',
   }
 headers = {
       'Accepts': 'application/json',
       'X-CMC_PRO_API_KEY': data["key"],
+      'Accept-Encoding': 'deflate, gzip',
   }
 
 session = Session()
 session.headers.update(headers)
+pp = pprint.PrettyPrinter(indent=4)
 
 try:
     response = session.get(url, params=parameters)
     data = json.loads(response.text)
-    print(data)
+    pp.pprint(data)
 except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
+    pp.pprint(e)
